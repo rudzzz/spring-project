7cc10808b42a01b9d052f75c8c0eab4ec01060c0
rudzzz <rudsolza13@gmail.com>
2022-12-10 11:51:03 -0300

    code

--- src/main/java/com/project/userdept/entities/User.java
+++ src/main/java/com/project/userdept/entities/User.java
@@ -1,9 +1,26 @@
 package com.project.userdept.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String name;
 	private String email;
+	
+	@ManyToOne
+	@JoinColumn(name = "department_id")
 	private Department department;
 	
 	public User() {

0d362a87daa9b3af6a751a0a7a94d4f825b1e6ef
rudzzz <rudsolza13@gmail.com>
2022-12-13 19:12:16 -0300

    code

--- src/main/java/com/project/userdept/controllers/UserController.java
+++ src/main/java/com/project/userdept/controllers/UserController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +29,12 @@ public class UserController {
 	@GetMapping(value = "/{id}")
 	public User findById(@PathVariable Long id){
 		User result = userRepository.findById(id).get();
+		return result;
+	}
+
+	@PostMapping()
+	public User save(@RequestBody User user){
+		User result = userRepository.save(user);
 		return result;
 	}
 }
